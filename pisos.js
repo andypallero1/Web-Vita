ad648790068ea8e552c45ce0d7a1bb56bac4070d
andypallero1 <andrespallero6@gmail.com>
2023-09-02 18:08:34 -0300

    web-vita

---
+++ pisos.js
@@ -0,0 +1,39 @@
+var clic = 1;
+
+function divLogin(){ 
+
+   if(clic==1){
+
+   document.getElementById("primerplano").style.width = "50%";
+
+   clic = clic + 1;
+
+   } else{
+
+       document.getElementById("primerplano").style.width = "0%";      
+
+    clic = 1;
+
+   }   
+
+}
+
+var clic = 1;
+
+function divOpen(){ 
+
+   if(clic==1){
+
+   document.getElementById("segundoplano").style.width = "50%";
+
+   clic = clic + 1;
+
+   } else{
+
+       document.getElementById("segundoplano").style.width = "0%";      
+
+    clic = 1;
+
+   }   
+
+}
---
+++ unidades.js
@@ -0,0 +1,39 @@
+var clic = 1;
+
+function mostrar1(){ 
+
+   if(clic==1){
+
+   document.getElementById("container1").style.display = "block";
+
+   clic = clic + 1;
+
+   } else{
+
+       document.getElementById("container1").style.display = "none";      
+
+    clic = 1;
+
+   }   
+
+}
+
+var clic = 1;
+
+function mostrar2(){ 
+
+   if(clic==1){
+
+   document.getElementById("container2").style.display = "block";
+
+   clic = clic + 1;
+
+   } else{
+
+       document.getElementById("container2").style.display = "none";      
+
+    clic = 1;
+
+   }   
+
+}
